7f42a35d98b30213d6e5154bf01e1357cdd41971
LeeJeongSeok <ljs0429777@gmail.com>
2020-06-06 00:46:55 +0900

    add : 이미지 파일 추가 및 paint 메소드를 통해 이미지 파일 생성

--- src/com/company/GamePanel.java
+++ src/com/company/GamePanel.java
@@ -5,10 +5,23 @@ import java.awt.*;
 
 public class GamePanel extends JPanel {
 
+    private ImageIcon testImage;
+
     public GamePanel() {
 
         setBackground(Color.PINK);
         setBounds(250, 250, 100, 100);
 
+    }
+
+    public void paint(Graphics g) {
+        g.setColor(Color.WHITE);
+        g.drawRect(10, 10, 30, 30);
+
+        testImage = new ImageIcon("src/resources/enemy.png");
+        testImage.paintIcon(this, g, 10, 10);
+
+
+        g.dispose();
     }
 }
